55cc7b3749d4c161368dce0971be81d6ea472c5b
Simon-0711 <Simon.pavicic99@gmail.com>
2023-09-26 17:07:11 +0200

    Add hyperparameter tuning

--- benchmarking_emnist/hyperparameter.py
+++ benchmarking_emnist/hyperparameter.py
@@ -1,13 +1,20 @@
-EPOCHS = 10
-NUM_CLASSES = 11
-BLANK_LABEL = 10
-IMAGE_HEIGHT = 28
-GRU_HIDDEN_SIZE = 128
-GRU_NUM_LAYERS = 2
-CNN_OUTPUT_HEIGHT = 4
-CNN_OUTPUT_WIDTH = 32
-DIGITS_PER_SEQUENCE = 5
-NUMBER_OF_SEQUENCES = 10000
-LEARNING_RATE = 0.001
-BATCH_SIZE = 64
-BATCH_SIZE_VAL = 1
+# Common hyperparameters
+common_hyperparameters = {
+    "NUM_CLASSES": 11,
+    "BLANK_LABEL": 10,
+    "IMAGE_HEIGHT": 28,
+    "DIGITS_PER_SEQUENCE": 5,
+    "NUMBER_OF_SEQUENCES": 1000,  # 10000
+    "EPOCHS": 2,
+}
+
+# Model-specific hyperparameters
+model_specific_hyperparameters = {
+    "GRU_HIDDEN_SIZE": [128],
+    "GRU_NUM_LAYERS": [2],
+    "CNN_OUTPUT_HEIGHT": [4],
+    "CNN_OUTPUT_WIDTH": [32],
+    "LEARNING_RATE": [1e-2, 1e-3, 1e-4],
+    "BATCH_SIZE": [32, 64, 128],
+    "BATCH_SIZE_VAL": [1],
+}
--- benchmarking_emnist/training/trainer.py
+++ benchmarking_emnist/training/trainer.py
@@ -5,13 +5,22 @@ from colorama import Fore
 from itertools import groupby
 from torchmetrics.text import CharErrorRate
 import numpy as np
-import hyperparameter as hp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+
 class Trainer:
     def __init__(
-        self, model, criterion, optimizer, train_loader, val_loader, test_loader, epochs, args, model_path=None
+        self,
+        model,
+        criterion,
+        optimizer,
+        train_loader,
+        val_loader,
+        test_loader,
+        epochs,
+        args,
+        model_path=None,
     ):
         self.model = model
         self.criterion = criterion
@@ -35,6 +44,7 @@ class Trainer:
         self.all_val_cers = []
         self.epoch_train_cers = []
         self.epoch_val_cers = []
+        self.epoch_test_cers = []
 
     def load_model(self, model_path):
         self.model.load_state_dict(torch.load(model_path))
@@ -63,7 +73,9 @@ class Trainer:
             loss = self.criterion(y_pred, y_train, input_lengths, target_lengths)
             loss.backward()
             self.optimizer.step()
-            train_correct_incr, train_total_incr = self.get_batch_cers(y_pred, y_train, batch_size)
+            train_correct_incr, train_total_incr = self.get_batch_cers(
+                y_pred, y_train, batch_size
+            )
             train_correct += train_correct_incr
             train_total += train_total_incr
         train_accuracy = train_correct / train_total
@@ -82,11 +94,7 @@ class Trainer:
         for i in range(batch_size):
             raw_prediction = list(max_index[:, i].detach().cpu().numpy())
             prediction = torch.IntTensor(
-                [
-                    c
-                    for c, _ in groupby(raw_prediction)
-                    if c != self.args["blank_label"]
-                ]
+                [c for c, _ in groupby(raw_prediction) if c != self.args["blank_label"]]
             )
 
             # check if predictions are empty and generate str list
@@ -132,7 +140,9 @@ class Trainer:
                 )
                 target_lengths = torch.IntTensor([len(t) for t in y_val])
                 self.criterion(y_pred, y_val, input_lengths, target_lengths)
-                val_correct_incr, val_total_incr = self.get_batch_cers(y_pred, y_val, batch_size)
+                val_correct_incr, val_total_incr = self.get_batch_cers(
+                    y_pred, y_val, batch_size
+                )
                 val_correct += val_correct_incr
                 val_total += val_total_incr
 
@@ -143,8 +153,8 @@ class Trainer:
         print(
             f"EPOCH {epoch + 1}/{self.epochs} - TESTING. Correct: {val_correct}/{val_total} = {val_accuracy:.4f} - Average CER Score: {round(np.mean(np.array(self.all_val_cers)), 3)}"
         )
-        
-    def test(self, plot_n = 1):
+
+    def test(self, plot_n=1):
         # Validation
         test_correct = 0
         test_total = 0
@@ -166,7 +176,9 @@ class Trainer:
                 )
                 target_lengths = torch.IntTensor([len(t) for t in y_test])
                 self.criterion(y_pred, y_test, input_lengths, target_lengths)
-                test_correct_incr, test_total_incr = self.get_batch_cers(y_pred, y_test, batch_size)
+                test_correct_incr, test_total_incr = self.get_batch_cers(
+                    y_pred, y_test, batch_size
+                )
                 test_correct += test_correct_incr
                 test_total += test_total_incr
 
@@ -177,19 +189,21 @@ class Trainer:
         print(
             f"TESTING. Correct: {test_correct}/{test_total} = {test_accuracy:.4f} - Average CER Score: {round(np.mean(np.array(self.all_val_cers)), 3)}"
         )
-        
-
 
         test_preds = []
         (x_test, y_test) = next(iter(self.train_loader))
-        y_pred = self.model(x_test.view(x_test.shape[0], 1, x_test.shape[1], x_test.shape[2]))
+        y_pred = self.model(
+            x_test.view(x_test.shape[0], 1, x_test.shape[1], x_test.shape[2])
+        )
 
         # Prepare plotting results
         y_pred = y_pred.permute(1, 0, 2)
         _, max_index = torch.max(y_pred, dim=2)
         for i in range(x_test.shape[0]):
             raw_prediction = list(max_index[:, i].detach().cpu().numpy())
-            prediction = torch.IntTensor([c for c, _ in groupby(raw_prediction) if c != hp.BLANK_LABEL])
+            prediction = torch.IntTensor(
+                [c for c, _ in groupby(raw_prediction) if c != self.args["BLANK_LABEL"]]
+            )
             test_preds.append(prediction)
 
         for j in range(len(x_test)):
@@ -197,12 +211,11 @@ class Trainer:
                 break
 
             mpl.rcParams["font.size"] = 8
-            plt.imshow(x_test[j], cmap='gray')
+            plt.imshow(x_test[j], cmap="gray")
             mpl.rcParams["font.size"] = 18
             plt.gcf().text(x=0.1, y=0.1, s="Actual: " + str(y_test[j].numpy()))
             plt.gcf().text(x=0.1, y=0.2, s="Predicted: " + str(test_preds[j].numpy()))
             plt.show()
-
 
     def train_validate_test(self):
         for epoch in range(self.epochs):
